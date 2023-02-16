d38b847a1abaa6b47f2d5b17b8e9f80af31a7ddf
Pavel <pashtet.net5@gmail.com>
2023-02-17 02:23:14 +0300

    feat:solution function chech in file index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let brackets=bracketsConfig.flat().join('')
+    let stack = [];
+for (let i=0;i<str.length;i++) {
+        let bracketsIndex = brackets.indexOf(str[i])         
+        if (bracketsIndex % 2 === 0) {               
+            if (str[i] === brackets[bracketsIndex + 1] && stack[stack.length - 1] === bracketsIndex){                  
+                stack.pop();
+            } else if (str[i] === brackets[bracketsIndex + 1] && stack[stack.length - 1] !== bracketsIndex) {
+                stack.push(bracketsIndex)
+            }
+            else{
+                stack.push(bracketsIndex)
+            }
+        } 
+        else {
+            if (stack.pop() !== bracketsIndex-1){
+                return false;
+            }
+        }           
+    }
+    return stack.length === 0
 }
